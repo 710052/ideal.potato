302ed151509e3b3050425de1cfca74ca56f3ea77
710052 <146425433+710052@users.noreply.github.com>
2024-02-14 02:50:00 +0000

    Added example dataset [names].

--- dataset.py
+++ dataset.py
@@ -1,7 +1,56 @@
 # Provided dataset
 data = """
-
-
+Lucas
+Benjamin
+Amelia
+Olivia
+Liam
+James
+Amelia
+James
+William
+Emma
+Ava
+Amelia
+Harper
+Benjamin
+Elijah
+Benjamin
+Amelia
+Harper
+Olivia
+William
+Harper
+Lucas
+James
+Elijah
+Liam
+Isabella
+Sophia
+Charlotte
+Mason
+Mia
+Amelia
+Elijah
+Benjamin
+Mason
+James
+Mia
+James
+Olivia
+Olivia
+Lucas
+Olivia
+Ava
+Noah
+James
+Lucas
+Benjamin
+Sophia
+Harper
+Ava
+Mia
+"""
 # Splitting the data into lines and removing any empty lines
 lines = data.strip().split('\n')
 
